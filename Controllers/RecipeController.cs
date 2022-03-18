c4301eb6e535ee301aca863dc469f943a93300de
fisher60 <wakefollower@gmail.com>
2022-03-17 23:02:10 -0500

    add sample api endpoint for model

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -7,4 +7,17 @@ namespace Reciplease.Controllers;
 public class RecipeController : ControllerBase
 {
 
+    private readonly ILogger<RecipeController> _logger;
+
+    public RecipeController(ILogger<RecipeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetRecipes")]
+    public string GetRecipes()
+    {
+        return "test";
+    }
+
 }
