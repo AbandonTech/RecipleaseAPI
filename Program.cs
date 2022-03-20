84a042b80ae366496e31f6df63aeedf911786ba8
GDWR <gregory.dwr@gmail.com>
2022-03-20 14:30:55 +0000

    Use lowercase urls for controllers

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddDbContext<RecipleaseContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 
 var app = builder.Build();
 
@@ -21,5 +23,4 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
