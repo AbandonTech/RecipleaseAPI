daeb49fff58a8e2813d629d6a4cc70aacda3a396
GDWR <gregory.dwr@gmail.com>
2022-03-20 16:46:33 +0000

    Add `Recipe` CRUD methods

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Reciplease.Controllers;
 
-[ApiController]
-[Route("[controller]")]
+[ApiController, Route("api/[controller]")]
 public class RecipeController : ControllerBase
 {
-
     private readonly ILogger<RecipeController> _logger;
     private readonly RecipleaseContext _context;
 
@@ -16,10 +15,90 @@ public class RecipeController : ControllerBase
         _context = context;
     }
 
-    [HttpGet(Name = "GetRecipes")]
-    public Recipe[] GetRecipes()
+    [HttpGet]
+    public ActionResult<Recipe[]> GetAllRecipes()
     {
         return _context.Recipes.ToArray();
     }
 
+    [HttpPost]
+    public ActionResult<Recipe> CreateRecipe(CreateRecipeDto recipe)
+    {
+        var newRecipe = new Recipe
+        {
+            Servings = recipe.Servings,
+            Name = recipe.Name
+        };
+
+        try
+        {
+            newRecipe = _context.Recipes.Add(newRecipe).Entity;
+            _context.SaveChanges();
+            return newRecipe;
+        }
+        catch (DbUpdateException e)
+        {
+            _logger.LogError(e, "Unable to add new recipe: {Recipe}", recipe.ToString());
+            return StatusCode(500);
+        }
+    }
+
+    [HttpGet, Route("{recipeId:int}")]
+    public ActionResult<Recipe> GetRecipe(int recipeId)
+    {
+        var recipe = _context.Recipes.Find(recipeId);
+
+        if (recipe == null)
+            return NotFound("a recipe with that id could not be found");
+
+        return recipe;
+    }
+
+    [HttpDelete, Route("{recipeId:int}")]
+    public ActionResult DeleteRecipe(int recipeId)
+    {
+        var recipeResult = GetRecipe(recipeId);
+
+        if (recipeResult.Value == null) return recipeResult.Result!;
+
+        _context.Recipes.Remove(recipeResult.Value!);
+        _context.SaveChanges();
+        return Ok();
+    }
+
+    [HttpPatch, Route("{recipeId:int}")]
+    public ActionResult<Recipe> UpdateRecipe(int recipeId, CreateRecipeDto recipe)
+    {
+        if (!_context.Recipes.Any(r => r.Id == recipeId))
+        {
+            return NotFound("a recipe with that id does not exist");
+        }
+
+        var newRecipe = new Recipe
+        {
+            Id = recipeId,
+            Servings = recipe.Servings,
+            Name = recipe.Name
+        };
+
+        var updatedRecipe = _context.Recipes.Update(newRecipe).Entity;
+        _context.SaveChanges();
+        return updatedRecipe;
+    }
+
+    [HttpPut]
+    public ActionResult<Recipe> UpdateOrCreateRecipe(Recipe recipe)
+    {
+        var oldRecipe = GetRecipe(recipe.Id).Value;
+        
+        if (oldRecipe == null)
+            return CreateRecipe(new CreateRecipeDto {Name = recipe.Name, Servings = recipe.Servings});
+
+        // Entity framework doesn't like us querying before hand, as it counts as a tracked entity.
+        // Due to this we have to clear the tracker before updating the entity of the same id.
+        _context.ChangeTracker.Clear();
+        recipe = _context.Recipes.Update(recipe).Entity;
+        _context.SaveChanges();
+        return recipe;
+    }
 }
--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -2,11 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Reciplease;
 
-public class Recipe 
+public class Recipe
 {
     public int Id { get; set; }
     public int Servings { get; set; }
 
-    [Required]
-    public string Name { get; set; }
+    [Required] public string Name { get; set; }
+}
+
+public class CreateRecipeDto
+{
+    public int Servings { get; set; }
+
+    public string Name { get; set; } = string.Empty;
 }
