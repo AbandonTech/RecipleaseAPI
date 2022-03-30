386727fb3ba72d48581275297aaecc8f23569c19
fisher60 <wakefollower@gmail.com>
2022-03-30 16:10:04 -0500

    add endpoint to login a user and issue a JWT
    This bypasses a lot of the .AddJwtBearer options. This should be changed in the future to better utilize the built-ins that I assume should be handling this with less manual configuration in the controller.

--- Contexts/RecipleaseContext.cs
+++ Contexts/RecipleaseContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Reciplease;
 
@@ -14,15 +15,38 @@ public class RecipleaseContext : IdentityDbContext<IdentityUser>
     public string? googleClientId = Environment.GetEnvironmentVariable("GOOGLE_OAUTH_CLIENT_ID");
     public string? googleClientSecret = Environment.GetEnvironmentVariable("GOOGLE_OAUTH_CLIENT_SECRET");
 
+    public string? JwtValidAudience = Environment.GetEnvironmentVariable("ValidAudience");
+    public string? JwtValidIssuer = Environment.GetEnvironmentVariable("ValidIssuer");
+    public string? JwtSecret = Environment.GetEnvironmentVariable("Secret");
+
     public string DbPath { get; }
 
     public RecipleaseContext(DbContextOptions<RecipleaseContext> options) : base(options)
     {
-        if (googleClientId == null){
+        if (googleClientId == null)
+        {
             throw new ArgumentNullException(paramName: nameof(googleClientId), message: "Google OAuth Client Id cannot be null.");
         }
-        if (googleClientSecret == null){
+        if (googleClientSecret == null)
+        {
             throw new ArgumentNullException(paramName: nameof(googleClientSecret), message: "Google OAuth Client Secret cannot be null.");
+        }
+
+        if (JwtValidAudience == null)
+        {
+            throw new ArgumentNullException(paramName: nameof(JwtValidAudience),
+                message: "JWT ValidAudience cannot be null.");
+        }
+
+        if (JwtValidIssuer == null)
+        {
+            throw new ArgumentNullException(paramName: nameof(JwtValidIssuer),
+                message: "JWT ValidIssuer cannot be null.");
+        }
+
+        if (JwtSecret == null)
+        {
+            throw new ArgumentNullException(paramName: nameof(JwtSecret), message: "Jwt Secret cannot be null.");
         }
 
         var CurrentDirectory = Environment.CurrentDirectory;
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,13 @@
-
 using Google.Apis.Auth;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 
 namespace Reciplease.Controllers;
@@ -10,13 +15,18 @@ namespace Reciplease.Controllers;
 [ApiController, Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-     private readonly ILogger<UserController> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly ILogger<UserController> _logger;
     private readonly RecipleaseContext _context;
 
-    public UserController(ILogger<UserController> logger, RecipleaseContext context)
+    public UserController(
+        ILogger<UserController> logger, 
+        RecipleaseContext context,
+        UserManager<IdentityUser> userManager)
     {
         _logger = logger;
         _context = context;
+        _userManager = userManager;
     }
 
     public class AuthenticateRequest
@@ -35,5 +45,51 @@ public class UserController : ControllerBase
 
         GoogleJsonWebSignature.Payload payload = GoogleJsonWebSignature.ValidateAsync(data.IdToken, settings).Result;
         return Ok( payload );
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = GetToken(authClaims);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
+
+        return Unauthorized();
+    }
+
+    private JwtSecurityToken GetToken(List<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_context.JwtSecret));
+
+        var token = new JwtSecurityToken(
+            issuer: _context.JwtValidIssuer,
+            audience: _context.JwtValidAudience,
+            expires: DateTime.Now.AddHours(3),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+        return token;
     }
 }
