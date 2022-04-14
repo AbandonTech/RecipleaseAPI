5e0c4a39d918eed056eb47142be5b20c4911a6df
GDWR <gregory.dwr@gmail.com>
2022-04-14 20:39:59 +0100

    Remove HTTPS
    Will be running behind cloudflare or nginx (which can be used to manage certs) so isn't really a problem. It makes deployment alot easier.

--- Program.cs
+++ Program.cs
@@ -23,14 +23,9 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
